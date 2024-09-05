5bb709adcf6774219662fc04015b7538217ea8c6
Saumya-1306 <156705797+Saumya-1306@users.noreply.github.com>
2024-09-06 01:49:31 +0530

    Add files via upload

---
+++ Module_3g-AJ--main/Module_3G/src/java/ejb/CountServletHitsBean.java
@@ -0,0 +1,15 @@
+
+package ejb;
+
+import javax.ejb.Singleton;
+
+
+@Singleton
+public class CountServletHitsBean {
+
+    private int hitCount;
+    
+    public synchronized int incrementAndGetHitCount(){
+        return hitCount++;
+    }
+}
